ede2aeb912769cb4145e17d134383f50767a5b12
Sohaib Ilyas (Roomi) <sohaibilyas@users.noreply.github.com>
2015-10-11 15:20:20 +0500

    Create login-callback.php

---
+++ login-callback.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+require_once __DIR__ . '/src/Facebook/autoload.php';
+
+$fb = new Facebook\Facebook([
+  'app_id' => 'APP_ID',
+  'app_secret' => 'APP_SECRET',
+  'default_graph_version' => 'v2.5'
+]);
+
+$helper = $fb->getJavaScriptHelper();
+
+try {
+  $accessToken = $helper->getAccessToken();
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+}
+
+if (isset($accessToken)) {
+   $fb->setDefaultAccessToken($accessToken);
+
+  try {
+  
+    $requestProfile = $fb->get("/me?fields=name,email");
+    $profile = $requestProfile->getGraphNode()->asArray();
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  }
+
+  $_SESSION['name'] = $profile['name'];
+  header('location: ../');
+  exit;
+} else {
+    echo "Unauthorized access!!!";
+    exit;
+}
